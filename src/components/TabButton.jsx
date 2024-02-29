6301761ea206afef08b52f4baca3de5d919cc6bc
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2024-02-29 15:12:52 +0200

    Not used function has been removed from TabButton Component

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,4 @@
 const TabButton = ({ children, isSelected, ...props }) => {
-  function handleClick() {
-    console.log("Clicked");
-  }
   return (
     <li>
       <button className={isSelected ? "active" : undefined} {...props}>
